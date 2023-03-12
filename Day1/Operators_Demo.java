ae08b36b59d0e720293308d8d1a2c46e85a13f86
AdityaVshk100 <121242508+AdityaVshk100@users.noreply.github.com>
2023-03-12 06:26:49 +0530

    Update Operators_Demo.java

--- Day1/Operators_Demo.java
+++ Day1/Operators_Demo.java
@@ -6,35 +6,38 @@ public class Operators_Demo {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-//        int c = (++a + b);
-//        System.out.println(c);
+        
+        // ---------++a or a++
+        int c = (++a + b);
+        System.out.println(c);
 
-
-//        System.out.println("Addition"+" "+(a+b)); // +
-//        System.out.println("Subtraction"+" "+(a-b)); // -
-//        System.out.println("Multiplication"+" "+" "+(a*b)); // *
-//        System.out.println("Modulus"+" "+(a%b)); // %
-//        System.out.println("Division"+" "+(a/b)); // /
+        //--------- Arthmetic operator
+        System.out.println("Addition"+" "+(a+b)); // +
+        System.out.println("Subtraction"+" "+(a-b)); // -
+        System.out.println("Multiplication"+" "+" "+(a*b)); // *
+        System.out.println("Modulus"+" "+(a%b)); // %
+        System.out.println("Division"+" "+(a/b)); // /
 
 
         // ------- Assignment Operator
-//        a+=20;
-//        System.out.println(a);
+        a+=20;
+        System.out.println(a);
 //
-//        a*=20;
-//        System.out.println(a);
+        a*=20;
+        System.out.println(a);
 //
-//        a/=20;
-//        System.out.println(a);
-//
-//        a%=20;
-//        System.out.println(a);
-//
-//        a-=20;
-//        System.out.println(a);
+        a/=20;
+        System.out.println(a);
 
-        // Relational Operator
+        a%=20;
+        System.out.println(a);
+//
+        a-=20;
+        System.out.println(a);
 
+        
+        
+        // ---------Relational Operator
         System.out.println(a<b);
         System.out.println(a>b);
         System.out.println(a<=b);
