d406612e1e2c674a8953cd91cddb0ffb84ce3cc1
AdityaVshk100 <121242508+AdityaVshk100@users.noreply.github.com>
2023-03-31 23:39:31 +0530

    Add files via upload

---
+++ Lecture_1/Demo_1.java
@@ -0,0 +1,15 @@
+package Lecture_01;
+
+public class Demo_1 {
+
+	public static void main(String[] args) {
+
+	int a = 10;
+	int b = 90;
+	int c = 40;
+	System.out.println(a+b+c);
+	}
+    
+    
+}
+
